1be69f2a19fc30c86568d16eedf75b4927b1b13c
swabyears <sam.w.byers@gmail.com>
2015-05-01 10:14:32 -0400

    modified example to fit changed in ParamikoHelper

--- example.py
+++ example.py
@@ -1,13 +1,16 @@
 import ParamikoHelper
 
-ip = '10.0.0.1' 
+ip1 = '10.0.0.1'
+ip2 = '10.0.0.2'
 username = 'uname'
 password = 'pword' 
 
-conn_string = "%s,%s,%s" % (ip,username,password)
+host1 = "%s,%s,%s" % (ip1,username,password)
+host2 = "%s,%s,%s" % (ip2,username,password)
 
 ssh = ParamikoHelper.ConnectToDevice() 
-ssh.add_host(conn_string) 
+ssh.add_host(host1)
+ssh.add_host(host2)
 ssh.connect()
 output = ssh.command("show version") 
 print output 
